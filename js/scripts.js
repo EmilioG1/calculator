24033591a7bf003a300021d17e6a8fe2e03d9421
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-21 13:22:49 -0700

    Add other operator functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+// function add(number1, number2) {
+// 	return number1 + number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+
+// const result = add(number1, number2);
+// alert(result);
+
 function add(number1, number2) {
-	return number1 + number2;
+	return number1 % number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1,number2));
+const result = add(number1, number2);
+alert(result);
