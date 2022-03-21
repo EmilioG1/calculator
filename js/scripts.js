4ab7312fcf2258cce57624116e73b4d56159c345
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-21 13:37:00 -0700

    Add a temp conversion function

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,10 @@
 // const result = add(number1, number2);
 // alert(result);
 
-function add(number1, number2) {
-	return number1 % number2;
+function temp(num) {
+	return ((num - 32) * (5 / 9));
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-const result = add(number1, number2);
-alert(result);
+const num = parseInt(prompt("Enter a temperature in Fahrenheit:"));
+const result = temp(num);
+alert(result.toFixed(1) + " degrees C");
