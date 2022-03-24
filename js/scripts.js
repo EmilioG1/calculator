2dab943600ebd69a6f7d6078c7167bba03c193bd
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-24 15:31:04 -0700

    Add calculator functionality and css
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,54 +19,22 @@ function divide(number1, number2) {
 //user logic
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#add1").val());
-		const number2 = parseInt($("#add2").val());
-		const result = add(number1, number2);
-		$("#outputAdd").text(result);
-	});
-
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#sub1").val());
-		const number2 = parseInt($("#sub2").val());
-		const result = subtract(number1, number2);
-		$("#outputSub").text(result);
-	});
-
-	$("form#multiply").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#multi1").val());
-		const number2 = parseInt($("#multi2").val());
-		const result = multiply(number1, number2);
-		$("#outputMult").text(result);
-	});
-
-	$("form#divide").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#div1").val());
-		const number2 = parseInt($("#div2").val());
-		const result = divide(number1, number2);
-		$("#outputDiv").text(result);
-	});
-
-
-
-
-	
+	$("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });	
 });
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-
-// const result = add(number1, number2);
-// alert(result);
-
-// function temp(num) {
-// 	return ((num - 32) * (5 / 9));
-// }
-
-// const num = parseInt(prompt("Enter a temperature in Fahrenheit:"));
-// const result = temp(num);
-// alert(result.toFixed(1) + " degrees C");
 
